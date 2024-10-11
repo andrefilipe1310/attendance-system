4c9c3f07fa4c778235ac0a4e9e2b5e6fa29a5b06
andrefilipe1310 <andrefilipef1310@gmail.com>
2024-10-11 18:19:53 -0300

    java update 7

--- src/main/java/com/etepam/attendance_system/AttendanceSystemApplication.java
+++ src/main/java/com/etepam/attendance_system/AttendanceSystemApplication.java
@@ -1,9 +1,18 @@
 package com.etepam.attendance_system;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Attendance System API",
+				version = "1.0",
+				description = "API para o sistema de gerenciamento de frequência de alunos do ensino médio"
+		)
+)
 public class AttendanceSystemApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/etepam/attendance_system/config/WebSecurityConfig.java
+++ src/main/java/com/etepam/attendance_system/config/WebSecurityConfig.java
@@ -32,7 +32,9 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(authorize->{
                     authorize.requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
                             .requestMatchers(HttpMethod.POST,"/auth/register").permitAll()
+                            .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                             .requestMatchers(HttpMethod.POST,"/student").hasRole("ADMIN")
+
                             .anyRequest().authenticated();
                 })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
