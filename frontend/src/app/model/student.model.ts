8ed3ced5289b959cc5220cb56f190b6d2b9dff75
AyrtonF <ayrtonleonardo14@gmail.com>
2024-12-04 23:16:23 -0300

    ajustando frontend e criando design inicial da tela de reconhecimento facial

--- frontend/src/app/model/student.model.ts
+++ frontend/src/app/model/student.model.ts
@@ -1,24 +1,23 @@
 export type Student = {
-    id?: number;            // O ? indica que o campo é opcional (para quando criamos um novo estudante)
-    name: string;          // Nome do estudante
+    id?: number;            
+    name: string;         
     email:string
     password:string
     phone:string
-    studentImage?: string; // Caminho para a imagem do estudante (opcional)
-    birth: string;         // Data de nascimento no formato ISO (YYYY-MM-DD)
-    mother: Guardian;      // Dados da mãe
-    father: Guardian;      // Dados do pai
-    absences?: Absence[];  // Lista de faltas (opcional)
+    studentImage?: string; 
+    birth: string;         
+    guardians:Guardian[];
+    absences?: Absence[];  
 }
 
 export interface Guardian {
-    id?: number;          // ID do responsável (opcional)
-    name: string;         // Nome do responsável
-    phone: string;        // Telefone do responsável
+    id?: number;          
+    name: string;        
+    phone: string;        
 }
 
 export interface Absence {
-    id?: number;          // ID da falta (opcional)
-    date: string;         // Data da falta
-    justification?: string; // Justificativa da falta (opcional)
+    id?: number;          
+    date: string;         
+    justification?: string; 
 }
