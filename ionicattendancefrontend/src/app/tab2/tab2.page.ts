fbd20e251c72c337a86524b5103dbcea3d2036a7
ArianoSouza <145685955+ArianoSouza@users.noreply.github.com>
2024-12-06 12:26:08 -0300

    Update tab2.page.ts

--- ionicattendancefrontend/src/app/tab2/tab2.page.ts
+++ ionicattendancefrontend/src/app/tab2/tab2.page.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+highlightedDates = [
+    {
+      date: '2023-01-05',
+      textColor: '#800080',
+      backgroundColor: '#ffc0cb',
+    },
+    {
+      date: '2023-01-10',
+      textColor: '#09721b',
+      backgroundColor: '#c8e5d0',
+    }
+  ];
   constructor() {}
 
 }
